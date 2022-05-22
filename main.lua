37c5c429033b9cad8650823f5e6116d30b12b69d
Synth <72616658+synthofficial@users.noreply.github.com>
2022-05-22 21:40:41 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,103 +1,101 @@
-local WindowTable = {}
+local WindowTable = {} -- We will be inserting Main Function in here
 
-function WindowTable:Create()
+function WindowTable:CreateWindow()
 	
-	-- Gui to Lua
-	-- Version: 3.2
-
-	-- Instances:
-
-	local Main = Instance.new("ScreenGui")
-	local MainWindow = Instance.new("Frame")
+	local Tutorial = Instance.new("ScreenGui")
+	local MainFrame = Instance.new("Frame")
 	local mainCorner = Instance.new("UICorner")
 	local SideBar = Instance.new("Frame")
 	local sideCorner = Instance.new("UICorner")
+	local coverCorner = Instance.new("Frame")
 	local allTabs = Instance.new("Frame")
 	local tabListing = Instance.new("UIListLayout")
-	local allPages = Instance.new("ScrollingFrame")
+	local allPages = Instance.new("Frame")
 	local mainCorner_2 = Instance.new("UICorner")
-	local pagesFolder = Instance.new("Folder")
 
-	--Properties:
+	Tutorial.Name = "Tutorial"
+	Tutorial.Parent = game.CoreGui	
+	Tutorial.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+	Tutorial.ResetOnSpawn = false
 
-	Main.Name = "Main"
-	Main.Parent = game.CoreGui
-	Main.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+	MainFrame.Name = "MainFrame"
+	MainFrame.Parent = Tutorial
+	MainFrame.BackgroundColor3 = Color3.fromRGB(45, 48, 53)
+	MainFrame.Position = UDim2.new(0.396969676, 0, 0.302184463, 0)
+	MainFrame.Size = UDim2.new(0, 430, 0, 260)
 
-	MainWindow.Name = "MainWindow"
-	MainWindow.Parent = Main
-	MainWindow.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
-	MainWindow.Position = UDim2.new(0.349328071, 0, 0.311360478, 0)
-	MainWindow.Size = UDim2.new(0, 550, 0, 268)
-
+	mainCorner.CornerRadius = UDim.new(0, 3)
 	mainCorner.Name = "mainCorner"
-	mainCorner.Parent = MainWindow
+	mainCorner.Parent = MainFrame
 
 	SideBar.Name = "SideBar"
-	SideBar.Parent = MainWindow
-	SideBar.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-	SideBar.Position = UDim2.new(0.0145454546, 0, 0.12686567, 0)
-	SideBar.Size = UDim2.new(0, 121, 0, 225)
+	SideBar.Parent = MainFrame
+	SideBar.BackgroundColor3 = Color3.fromRGB(35, 38, 42)
+	SideBar.BorderSizePixel = 0
+	SideBar.Size = UDim2.new(0, 110, 0, 260)
 
+	sideCorner.CornerRadius = UDim.new(0, 3)
 	sideCorner.Name = "sideCorner"
 	sideCorner.Parent = SideBar
 
+	coverCorner.Name = "coverCorner"
+	coverCorner.Parent = SideBar
+	coverCorner.BackgroundColor3 = Color3.fromRGB(35, 38, 42)
+	coverCorner.BorderSizePixel = 0
+	coverCorner.Position = UDim2.new(0.943925261, 0, 0, 0)
+	coverCorner.Size = UDim2.new(0, 6, 0, 260)
+
 	allTabs.Name = "allTabs"
 	allTabs.Parent = SideBar
-	allTabs.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-	allTabs.Position = UDim2.new(0.0702479333, 0, 0.0377777778, 0)
-	allTabs.Size = UDim2.new(0, 104, 0, 210)
+	allTabs.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	allTabs.BackgroundTransparency = 1.000
+	allTabs.Position = UDim2.new(0.0500000007, 0, 0.0299999993, 0)
+	allTabs.Size = UDim2.new(0, 98, 0, 246)
 
 	tabListing.Name = "tabListing"
 	tabListing.Parent = allTabs
-	tabListing.HorizontalAlignment = Enum.HorizontalAlignment.Center
 	tabListing.SortOrder = Enum.SortOrder.LayoutOrder
-	tabListing.Padding = UDim.new(10, 10)
+	tabListing.Padding = UDim.new(0, 2)
 
 	allPages.Name = "allPages"
-	allPages.Parent = MainWindow
-	allPages.Active = true
-	allPages.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-	allPages.Position = UDim2.new(0.246234685, 0, 0.13059701, 0)
-	allPages.Size = UDim2.new(0.742856085, 0, 0.835821033, 0)
-	allPages.ScrollBarThickness = 4
+	allPages.Parent = MainFrame
+	allPages.BackgroundColor3 = Color3.fromRGB(39, 42, 47)
+	allPages.Position = UDim2.new(0.274418592, 0, 0.0269230772, 0)
+	allPages.Size = UDim2.new(0, 306, 0, 245)
 
-	mainCorner_2.CornerRadius = UDim.new(0, 100)
+	mainCorner_2.CornerRadius = UDim.new(0, 3)
 	mainCorner_2.Name = "mainCorner"
 	mainCorner_2.Parent = allPages
 
+	local pagesFolder = Instance.new("Folder")
+
 	pagesFolder.Name = "pagesFolder"
 	pagesFolder.Parent = allPages
-
 	
 	local TabHandler = {}
 	
-	function TabHandler:Tab(tabName)
-		tabName = tabName or "New Tab"
-		
-		local tabButton = Instance.new("TextButton") -- our tab
-		local UICorner = Instance.new("UICorner")-- our tab corner
-		local newPage = Instance.new("ScrollingFrame") -- our page
-		local elementsListing = Instance.new("UIListLayout") -- our page listing
-		
+	function TabHandler:CreateTab(tabname)
+		tabname = tabname or "New Tab"
+		local tabButton = Instance.new("TextButton")
+		local tabCorner = Instance.new("UICorner")
+		local newPage = Instance.new("ScrollingFrame") 
+		local elementsListing = Instance.new("UIListLayout") 
 		
 		tabButton.Name = "tabButton"
 		tabButton.Parent = allTabs
-		tabButton.AnchorPoint = Vector2.new(0, 0.100000001)
-		tabButton.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-		tabButton.BackgroundTransparency = 0.300
-		tabButton.Position = UDim2.new(-0.0192307699, 0, 0.0152221676, 0)
-		tabButton.Size = UDim2.new(0.1053719, 109, 0.00999999978, 32)
-		tabButton.Font = Enum.Font.JosefinSans
-		tabButton.Text = "Example"
+		tabButton.BackgroundColor3 = Color3.fromRGB(115, 49, 37)
+		tabButton.Size = UDim2.new(0, 99, 0, 25)
+		tabButton.Font = Enum.Font.Gotham
+		tabButton.Text = tabname -- Displays our custom text
 		tabButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 		tabButton.TextSize = 14.000
+		tabButton.AutoButtonColor = false	
 		tabButton.MouseButton1Click:Connect(function()
 			for i,v in next, pagesFolder:GetChildren() do -- We get all the pages that we added
 				v.Visible = false	-- then we make them invisible 
 			end 
 			newPage.Visible = true	-- We make current page visible but not others
-
+			
 			--Animations Below  -- Optional
 			for i,v in next, allTabs:GetChildren() do	-- We get all the elements inside the frame
 				if v:IsA("TextButton") then -- We can't animate UIListLayout, so we check if its a TextButton
@@ -110,25 +108,27 @@ function WindowTable:Create()
 				BackgroundColor3 = Color3.fromRGB(255, 109, 83) -- We animate other Tab Buttons and making the current one seem Checked
 			}):Play()
 		end)
-		
-		UICorner.CornerRadius = UDim.new(0, 3)
-		UICorner.Name = "sideCorner"		
-		UICorner.Parent = tabButton
+
+		tabCorner.CornerRadius = UDim.new(0, 3)
+		tabCorner.Name = "sideCorner"
+		tabCorner.Parent = tabButton
 		
 		newPage.Name = "newPage"
-		newPage.Parent = allPages
+		newPage.Parent = pagesFolder
 		newPage.Active = true
-		newPage.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-		newPage.Size = UDim2.new(0, 408, 0, 224)
-		newPage.ScrollBarThickness = 4
-		
+		newPage.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		newPage.BackgroundTransparency = 1.000
+		newPage.Size = UDim2.new(1, 0, 1, 0)
+		newPage.ScrollBarThickness = 5
 
 		elementsListing.Name = "elementsListing"
 		elementsListing.Parent = newPage
+		elementsListing.HorizontalAlignment = Enum.HorizontalAlignment.Center
 		elementsListing.SortOrder = Enum.SortOrder.LayoutOrder
-		
+		elementsListing.Padding = UDim.new(0, 5)
 		
 	end
+	
 	return TabHandler
 end
 
